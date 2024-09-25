cef2544349f7e3c7539b3317a830fc8cf17999bb
Jean Olmedillo <78714438+Jmofuture@users.noreply.github.com>
2024-09-25 20:36:46 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  randomCard();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById("newCardBtn").addEventListener("click", randomCard);
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomCard() {
+  const Symbols = ["♠", "♣", "♥", "♦"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomSymbolIndex = Math.floor(Math.random() * Symbols.length);
+  let randomValueIndex = Math.floor(Math.random() * values.length);
+
+  let randomSymbol = Symbols[randomSymbolIndex];
+  let randomValue = values[randomValueIndex];
+
+  let cardElement = document.getElementById("card");
+
+  cardElement.className = `shadow-sm p-3 mb-5 bg-white d-flex flex-column justify-content-between align-items-stretch text-center ${getSymbolClass(
+    randomSymbol
+  )}`;
+
+  document.querySelector(".top-symbol").innerHTML = randomSymbol;
+  document.querySelector(".value").innerHTML = randomValue;
+  document.querySelector(".bottom-symbol").innerHTML = randomSymbol;
+}
+
+function getSymbolClass(Symbol) {
+  const SymbolClasses = {
+    "♠": "text-dark",
+    "♣": "text-dark",
+    "♥": "text-danger",
+    "♦": "text-danger"
+  };
+
+  return SymbolClasses[Symbol];
+}
